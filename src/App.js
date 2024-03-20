1a1fc66709d78eead3f2227c9d54512adc7c9165
Greenleaf20 <archanaganes@umass.edu>
2024-03-20 15:07:46 -0400

    Add images for theme selection

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ import Social from './components/Social';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [darkMode, setDarkMode] = useState(
-    () => localStorage.getItem('darkMode') === 'true'
-  );
+  const [darkMode, setDarkMode] = useState( () => {
+    const darkmode = localStorage.getItem('darkMode');
+    console.log(darkmode)
+    return darkmode ? darkmode === 'true' : true;
+  });
 
   useEffect(() => {
     localStorage.setItem('darkMode', darkMode);
@@ -49,10 +51,23 @@ function App() {
             </li>
           </ul>
           <div className="d-flex justify-content-end align-items-center theme-slider-container">
-            <div className="form-check form-switch">
+            {/* <div className="form-check form-switch">
               <input className="form-check-input" type="checkbox" id="themeSwitch" onChange={toggleDarkMode} checked={darkMode} />
               <label className="form-check-label theme-text" htmlFor="themeSwitch" id="themeSwitchLabel">{darkMode ? 'Dark' : 'Light'} </label>
-            </div>
+            </div> */}
+            <div className="flex flex-col justify-center ml-3 form-check form-switch">
+              {/* <input type="checkbox" name="light-switch" className="light-switch sr-only"  id="themeSwitch" onChange={toggleDarkMode} checked={darkMode} /> */}
+              <label className="relative cursor-pointer p-2" htmlFor="light-switch" type="button">
+                  {darkMode && <svg className="dark:hidden" width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="#C0C0C0" onClick={toggleDarkMode}>
+                      <path className="fill-slate-300" d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z" />
+                      <path className="fill-slate-400" d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z" />
+                  </svg>}
+                  {!darkMode &&  <svg className="hidden dark:block" width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="#C0C0C0" onClick={toggleDarkMode}>
+                      <path className="fill-slate-400" d="M6.2 1C3.2 1.8 1 4.6 1 7.9 1 11.8 4.2 15 8.1 15c3.3 0 6-2.2 6.9-5.2C9.7 11.2 4.8 6.3 6.2 1Z" />
+                      <path className="fill-slate-500" d="M12.5 5a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25.625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25.625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 5Z" />
+                  </svg>}
+              </label>
+          </div>
           </div>
         </div>
       </nav>
