e59fe41829b1ff49024737d10e2ed85cd863b0d9
Greenleaf20 <archanaganes@umass.edu>
2024-03-20 16:59:36 -0400

    Add alt text to social

--- src/components/Social.js
+++ src/components/Social.js
@@ -7,22 +7,22 @@ function Social() {
                 One of my favorite authors is Robert Dugoni and I am a few books away from completing all his books. I am also a huge fan of legal thrillers of Michael Connelly and John Grisham. Every year I participate in the Goodreads reading challenge and one of my proudest years is 2021 when I read 61 books. Being a huge fan of the Lord of the Rings movies, I am currently reading the Tolkien's Lord of the Rings Trilogy.
             </p>
             <div className="social-pics-div">
-            <img className="social-pics-l" src="/assets/images/social/2021-reading.png"/>
-            <img className="social-pics-p" src="/assets/images/social/jane-austen-award.jpeg"/>
+            <img className="social-pics-l" alt="2021 reading challenge" src="/assets/images/social/2021-reading.png"/>
+            <img className="social-pics-p" alt="Jane Austen award" src="/assets/images/social/jane-austen-award.jpeg"/>
             </div>
             <p>
                 I <span className="heart">&#9829;</span> dogs and most animals. I grew up playing with and helping the needy stray dogs in my hometown Neyveli. As I matured, I realized the sufferings they endured on the streets and set out on a campaign for adoption of as many of the dogs as possible. Ever since, I have been an active monetary contributor and supporter of the Animal Birth Control (ABC) campaign. When I joined my undergraduate degree, I saw much of the same suffering there as well. A few of us like minded people at the university decided to start an Animal Welfare Club at the university which aimed at feeding, vaccinating and providing health care for stray animals in the area. With funds from many such enthusiasts at the university which included professors as well, we were able to install water pots in the area. With the generous contribution from BowChow SuperDog, we were able to feed nutritious food to the dogs at the university. Below are the pictures of me with my team and my furry friends. 
             </p>
             <div className="social-pics-div">
-            <img className="social-pics-p" src="/assets/images/social/archana-dogs.jpeg"/>
-            <img className="social-pics-l" src="/assets/images/social/animal-welfare-club.jpg"/>
+            <img className="social-pics-p" alt="archana with dogs" src="/assets/images/social/archana-dogs.jpeg"/>
+            <img className="social-pics-l" alt="animal welfare club" src="/assets/images/social/animal-welfare-club.jpg"/>
             </div>
             <p>
                 I am amazed by <span>&#129429;</span> and am a huge fan of the Jurassic Park and Jurassic World franchise. I visited the Dinosaur Footprints in Massachusetts which were left by some of the earliest known dinosaurs in the Connecticut River Valley region. Below is a picture of me at the historic area and at the North Carolina  museum of natural sciences with a T-Rex.
             </p>
             <div className="social-pics-div">
-            <img className="social-pics-p" src="/assets/images/social/nc-museum.jpeg"/>
-            <img className="social-pics-p" src="/assets/images/social/dino-footprint.jpeg"/>
+            <img className="social-pics-p" alt="NC museum of natural science" src="/assets/images/social/nc-museum.jpeg"/>
+            <img className="social-pics-p" alt="dino footprint reservation" src="/assets/images/social/dino-footprint.jpeg"/>
             </div>
         </div>
     );
