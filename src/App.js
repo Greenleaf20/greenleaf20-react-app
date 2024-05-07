2891eff67fc4ba34a73386764ef10c0836bec83c
Greenleaf20 <archanaganes@umass.edu>
2024-05-07 15:27:24 -0400

    Fix theme issue

--- src/App.js
+++ src/App.js
@@ -96,11 +96,11 @@ function App() {
             <div className="flex flex-col justify-center ml-3 form-check form-switch">
               {/* <input type="checkbox" name="light-switch" className="light-switch sr-only"  id="themeSwitch" onChange={toggleDarkMode} checked={darkMode} /> */}
               <label className="relative cursor-pointer p-2" htmlFor="light-switch" type="button">
-                  {darkMode && <svg className="dark:hidden" width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="#C0C0C0" onClick={toggleDarkMode}>
+                  {darkMode && <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="#C0C0C0" onClick={toggleDarkMode}>
                       <path className="fill-slate-300" d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z" />
                       <path className="fill-slate-400" d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z" />
                   </svg>}
-                  {!darkMode &&  <svg className="hidden dark:block" width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="#C0C0C0" onClick={toggleDarkMode}>
+                  {!darkMode &&  <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="#C0C0C0" onClick={toggleDarkMode}>
                       <path className="fill-slate-400" d="M6.2 1C3.2 1.8 1 4.6 1 7.9 1 11.8 4.2 15 8.1 15c3.3 0 6-2.2 6.9-5.2C9.7 11.2 4.8 6.3 6.2 1Z" />
                       <path className="fill-slate-500" d="M12.5 5a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25.625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25.625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 5Z" />
                   </svg>}
@@ -117,22 +117,22 @@ function App() {
             <p> 
               Master of Science <br/> <a href="https://www.cics.umass.edu/" className="text-secondary" target="_blank" rel="noreferrer">College of Information and Computer Sciences</a> <br/> University of Massachusetts Amherst
             </p>
-            <p class="info-class">
+            <p className="info-class">
               <img src={darkMode ? "/assets/images/svgs/location-logo-light.svg" : "/assets/images/svgs/location-logo.svg"}  id="location" alt="location" className="me-2"/>Amherst, MA, USA
             </p>
-            <p class="info-class">
+            <p className="info-class">
               <img src={darkMode ? "/assets/images/svgs/email-logo-light.svg" : "/assets/images/svgs/email-logo.svg"} className="me-2" id="email" alt="email" />archanaganes@umass.edu
             </p>
-            <p class="info-class">
+            <p className="info-class">
               <img src={darkMode ? "/assets/images/svgs/linkedin-logo-light.svg" : "/assets/images/svgs/linkedin-logo.svg"}  className="me-2" id="linkedin" alt="linkedin"/><a href="https://www.linkedin.com/in/ganesh-archana/" className="text-secondary" target="_blank" rel="noreferrer">ganesh-archana</a>
             </p>
-            <p class="info-class">
+            <p className="info-class">
               <img src={darkMode ? "/assets/images/svgs/scholar-logo-light.svg" : "/assets/images/svgs/scholar-logo.svg"}  className="me-2" id="google-scholar" alt="google-scholar"/><a href="https://scholar.google.com/citations?hl=en&user=JhFxgOoAAAAJ" className="text-secondary" target="_blank" rel="noreferrer">Archana Ganesh</a>
             </p>
-            <p class="info-class">
+            <p className="info-class">
               <img src={darkMode ? "/assets/images/svgs/github-logo-light.svg" : "/assets/images/svgs/github-logo.svg"}  className="me-2" id="github" alt="github"/><a href="https://github.com/Greenleaf20" className="text-secondary" target="_blank" rel="noreferrer">Greenleaf20</a>
             </p>
-            <p class="info-class">
+            <p className="info-class">
               <img src={darkMode ? "/assets/images/svgs/goodreads-logo-light.svg" : "/assets/images/svgs/goodreads-logo.svg"}  className="me-2" id="goodreads" alt="goodreads"/><a href="https://www.goodreads.com/_greenleaf" className="text-secondary" target="_blank" rel="noreferrer">_greenleaf</a>
             </p>
             <hr className="d-sm-none"/>
