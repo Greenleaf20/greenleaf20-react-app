109adc2cff151a109fbc0523f2bd73e0c92df629
Greenleaf20 <archanaganes@umass.edu>
2024-05-08 19:17:45 -0400

    Fix awards social img css

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -6,16 +6,16 @@ function Awards() {
             I was given the Takes Ownership award by Citi in March 2022 for my work developing Angular micro frontends in the Unified Desktop team. I spent 2 glorious years in the team and developed a great deal of technical as well as leadership skills. 
             </p>
             <div className="awards-div">
-            <img className="award" src="/assets/images/awards/takes-ownership.png" alt="takesownership"/>
-            <img className="award" src="/assets/images/awards/succeeds-together.png" alt="succeedstogether"/>
+                <img className="award" src="/assets/images/awards/takes-ownership.png" alt="takesownership"/>
+                <img className="award" src="/assets/images/awards/succeeds-together.png" alt="succeedstogether"/>
             </div>
             I was given a Certificate of Reviewing by Elsevier for my contribution to the Computers in Biology and Medicine journal. 
             <div className="awards-div">
-            <img className="award" src="/assets/images/awards/review-cert.png" alt="reviewcert"/>
+                <img className="award" src="/assets/images/awards/review-cert.png" alt="reviewcert"/>
             </div>
             I was the second among girls in the CBSE 10<sup>th</sup> national public examination in my school and I received the Academic Achievement Award and cash compensation as well. 
             <div className="awards-div">
-            <img className="award" src="/assets/images/awards/academic-achievement.jpeg" alt="academicachievement"/>
+                <img className="award" src="/assets/images/awards/academic-achievement.jpeg" alt="academicachievement"/>
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-const flowbite = require("flowbite-react/tailwind");
 
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    flowbite.content(),
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    flowbite.plugin(),
+    require('flowbite/plugin')
   ],
 }
 
