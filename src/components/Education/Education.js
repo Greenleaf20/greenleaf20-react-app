ee13451a2e58afb1a3d52d98a01ac2ca7e281cc3
Greenleaf20 <archanaganes@umass.edu>
2024-05-08 20:02:50 -0400

    Fix image ratio issue

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -6,83 +6,91 @@ function Education() {
             <h2 className="heading" id="education">Education</h2><hr/>
             <div className="d-flex justify-content-between">
                 <div className="d-flex">
-                <img src="/assets/images/logos/umass.png" alt="UMass Amherst" className="company-icon"/> 
-                <div className="d-flex ms-2">
-                    <h5>University of Massachusetts Amherst</h5>
-                </div>
+                    <img src="/assets/images/logos/umass.png" alt="UMass Amherst" className="company-icon"/> 
+                    <div className="d-flex ms-2">
+                        <h5>University of Massachusetts Amherst</h5>
+                    </div>
                 </div>
                 <em>Sep '22 - May '24</em>
             </div>
             <div className="container">
                 <div className="row">
-                <div className="col-6">
-                    <img src="/assets/images/university/umass.jpeg" alt="UMass Amherst" className="university-img left-align-media"/>
-                </div>
-                <div className="col-6">
-                    <div className="coursework">
-                    <b>Master of Science in Computer Science</b> (GPA: 3.83 / 4.0) <br/><br/>
-                    <b>Coursework:</b> Advanced Algorithms, Distributed and Operating Systems, Computer and Network Security, Theory and Practice of Software Engineering, System Defense and Test, Secure Distributed Systems, Machine Learning, Applied Information Retrieval, Systems for Data Science, Reinforcement Learning
+                    <div className="col-6">
+                        <div className="coursework">
+                            <b>Master of Science in Computer Science</b> (GPA: 3.83 / 4.0) <br/><br/>
+                            <b>Coursework:</b> Advanced Algorithms, Distributed and Operating Systems, Computer and Network Security, Theory and Practice of Software Engineering, System Defense and Test, Secure Distributed Systems, Machine Learning, Applied Information Retrieval, Systems for Data Science, Reinforcement Learning
+                        </div>
                     </div>
-                </div>
+                    <div className="col-6">
+                        <img src="/assets/images/university/umass.jpeg" alt="UMass Amherst" className="university-img right-align-media"/>
+                    </div>
                 </div>
             </div>
     
             <div className="d-flex justify-content-between">
                 <div className="d-flex">
-                <img src="/assets/images/logos/nitt.png" alt="NIT Trichy" className="company-icon" /> 
-                <div className="d-flex ms-2">
-                    <h5>National Institute of Technology Tiruchirappalli</h5>
-                </div>
+                    <img src="/assets/images/logos/nitt.png" alt="NIT Trichy" className="company-icon" /> 
+                    <div className="d-flex ms-2">
+                        <h5>National Institute of Technology Tiruchirappalli</h5>
+                    </div>
                 </div>
                 <em>July '16 - Jun '20</em>
             </div>
             <div className="container">
                 <div className="row">
-                <div className="col-6">
-                    <div className="coursework">
-                    <b>Bachelor of Technology in Instrumentation and Control Engineering</b> (GPA: 8.41 / 10.0)
-                    <br/><br/>
-                    <b>Minor in Computer Science and Engineering</b> (GPA: 8.6 / 10.0)
-                    <br/><br/>
-                    <b>Coursework:</b> Data Structures and Algorithms, Operating Systems, Database Management System, Computer Organization, Software Engineering, Big Data Analytics, Neural Networks and Fuzzy Logic, Digital Electronics, Microprocessors and Microcontrollers
+                    <div className="col-6">
+                        <div className="coursework">
+                            <b>Bachelor of Technology in Instrumentation and Control Engineering</b> (GPA: 8.41 / 10.0)
+                            <br/><br/>
+                            <b>Minor in Computer Science and Engineering</b> (GPA: 8.6 / 10.0)
+                            <br/><br/>
+                            <b>Coursework:</b> Data Structures and Algorithms, Operating Systems, Database Management System, Computer Organization, Software Engineering, Big Data Analytics, Neural Networks and Fuzzy Logic, Digital Electronics, Microprocessors and Microcontrollers
+                            <br/><br/>
+                            <b>Positions of Responsibility:</b>
+                            <br/>
+                            Manager of Public Relations and Logistics, Festember <br/>
+                            Manager of Public Relations and Logistics, Pragyan <br/>
+                            Publicity Cordinator of Sensors <br/>
+                            Member of Tamil Mandram <br/>
+                            Member of Animal Welfare Club
+                        </div>
                     </div>
-                </div>
-                <div className="col-6">
-                    <img src="/assets/images/university/nitt.jpeg" alt="NIT Trichy" className="university-img-portrait"/>
-                </div>
+                    <div className="col-6">
+                        <img src="/assets/images/university/nitt.jpeg" alt="NIT Trichy" className="university-img-portrait right-align-media"/>
+                    </div>
                 </div>
             </div>
     
             <div className="d-flex justify-content-between">
                 <div className="d-flex">
-                <img src="/assets/images/logos/jawahar.png" alt="Jawahar" className="company-icon" /> 
-                <div className="d-flex ms-2">
-                    <h5>Jawahar Higher Secondary School (CBSE) Neyveli</h5>
-                </div>
+                    <img src="/assets/images/logos/jawahar.png" alt="Jawahar" className="company-icon" /> 
+                    <div className="d-flex ms-2">
+                        <h5>Jawahar Higher Secondary School (CBSE) Neyveli</h5>
+                    </div>
                 </div>
                 <em>Jun '14 - May '16</em>
             </div>
             <div className="container">
                 <div className="row">
-                <div className="col-6">
-                    <img src="/assets/images/university/high-school.jpg" alt="Jawahar" className="university-img left-align-media"/>
-                </div>
-                <div className="col-6">
-                    <div className="coursework">
-                    94.8% in CBSE board examination conducted in 2016 and received Academic Achievement Award for the same <br/><br/>
-                    Secured an All India Rank of 9858 among 1.2 million students in JEE Mains 2016 <br/><br/>
-                    <b>Coursework:</b> Computer Science, Mathematics, Physics, Chemistry, English
+                    <div className="col-6">
+                        <div className="coursework">
+                            94.8% in CBSE board examination conducted in 2016 and received Academic Achievement Award for the same <br/><br/>
+                            Secured an All India Rank of 9858 among 1.2 million students in JEE Mains 2016 <br/><br/>
+                            <b>Coursework:</b> Computer Science, Mathematics, Physics, Chemistry, English
+                        </div>
                     </div>
-                </div>
+                    <div className="col-6">
+                        <img src="/assets/images/university/high-school.jpg" alt="Jawahar" className="university-img right-align-media"/>
+                    </div>
                 </div>
             </div>
 
             <h2 className="" id="academic">Academic Experience</h2><hr/>
             <div className="d-flex justify-content-between">
-            <div className="d-flex">    
-                <h5 className="pe-1">Grading Assistant </h5> - CS 520: Theory and Practice of Software Engineering
-            </div>
-            <em>Feb '24 - May '24</em>
+                <div className="d-flex">    
+                    <h5 className="pe-1">Grading Assistant </h5> - CS 520: Theory and Practice of Software Engineering
+                </div>
+                <em>Feb '24 - May '24</em>
             </div>
         </div>
     );
